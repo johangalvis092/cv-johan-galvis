7d06824a8b0274a36ae44de0aa14885e92a0865e
johangalvis092 <johangalvis102@gmail.com>
2025-06-09 18:32:46 +0000

    componente Perfil con descripción personalizada

---
+++ mi-app-react/src/components/Perfil.jsx
@@ -0,0 +1,18 @@
+// src/components/Perfil.jsx
+export default function Perfil () {
+  return (
+    <section>
+      <h2>Perfil Profesional</h2>
+      <p>
+        Soy un desarrollador en formación con enfoque en tecnologías Front-End. Me apasiona construir interfaces limpias y funcionales, aplicando buenas prácticas de código y trabajo en equipo. Disfruto aprendiendo constantemente nuevas herramientas y lenguajes.
+      </p>
+      <ul>
+        <li>🔧 HTML, CSS, JavaScript, React</li>
+        <li>🤝 Trabajo en equipo, comunicación asertiva</li>
+        <li>📚 Aprendizaje continuo</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Perfil;
