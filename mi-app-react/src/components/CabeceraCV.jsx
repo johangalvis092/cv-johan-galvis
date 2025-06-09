1b5a3c4134a1dc7e26f5edffdd96c4b47a4973fb
johangalvis092 <johangalvis102@gmail.com>
2025-06-09 18:27:11 +0000

    componente CabeceraCV con datos personales actualizados

---
+++ mi-app-react/src/components/CabeceraCV.jsx
@@ -0,0 +1,12 @@
+export default function CabeceraCV (){
+
+  return (
+    <header>
+      <h1>Johan Galvis</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+      <p>📞 +57 300 123 4567 | 📧 johangalvis102@email.com | 🏠 medellin, Colombia</p>
+    </header>
+  );
+};
+
+
