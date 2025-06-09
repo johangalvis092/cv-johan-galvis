99ddc51577209f9ac62dfd24303b3e47aa22c5c1
johangalvis092 <johangalvis102@gmail.com>
2025-06-09 18:43:12 +0000

    componente Educacion con formación complementaria

---
+++ mi-app-react/src/components/Educacion.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Experiencia() {
+  return (
+    <section>
+      <h2>Educación y Formación</h2>
+      <ul>
+        <li>Técnico en Programación de Software - SENA (2023)</li>
+        <li>Curso React desde Cero - Platzi (2024)</li>
+        <li>Seminario de Git y GitHub - Udemy (2024)</li>
+        <li>Taller de Metodologías Ágiles - SENA (2023)</li>
+      </ul>
+    </section>
+  );
+};
+
+
